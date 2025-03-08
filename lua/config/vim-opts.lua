0d490ba142b13ef822203b04a123165cb00e01ec
Evan <dagurevan@gmail.com>
2025-03-07 20:09:40 -0800

    Arch Linux build

---
+++ lua/config/vim-opts.lua
@@ -0,0 +1,22 @@
+-- tabstops, QoL, etc.
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
+-- keybinding for leader
+vim.g.mapleader = " "
+
+-- clipboard
+vim.g.clipboard = {
+    name = 'win32yank-wsl',
+    copy = {
+        ['+'] =  'win32yank.exe -i --crlf',
+        ['*'] =  'win32yank.exe -i --crlf',
+    },
+    paste = {
+        ['+'] = 'win32yank.exe -o --lf',
+        ['*'] = 'win32yank.exe -o --lf',
+    },
+    cache_enabled = true,
+}
