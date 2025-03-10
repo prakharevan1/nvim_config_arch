facc5ff84240d82a13422fa0ef4d8b26f0639a78
Evan <dagurevan@gmail.com>
2025-03-09 19:18:18 -0700

    Fixed commands in rustaceanvim cd'ing to wrong dir

--- lua/plugins/code_formatting/rustaceanvim.lua
+++ lua/plugins/code_formatting/rustaceanvim.lua
@@ -5,16 +5,21 @@ return {
 		lazy = false, -- This plugin is already lazy
 		config = function()
 			-- Cargo run
-			vim.keymap.set("n", "<leader>rr", ":cd %:p:h | !cargo run<CR>", { noremap = true, silent = true })
+			vim.keymap.set("n", "<leader>rr", ":cd %:p:h:h | !cargo run<CR>", { noremap = true, silent = true })
 
 			-- Cargo run --release
-			vim.keymap.set("n", "<leader>rR", ":cd %:p:h | !cargo run --release<CR>", { noremap = true, silent = true })
+			vim.keymap.set(
+				"n",
+				"<leader>rR",
+				":cd %:p:h:h | !cargo run --release<CR>",
+				{ noremap = true, silent = true }
+			)
 
 			-- Cargo test
-			vim.keymap.set("n", "<leader>rt", ":cd %:p:h | !cargo test<CR>", { noremap = true, silent = true })
+			vim.keymap.set("n", "<leader>rt", ":cd %:p:h:h | !cargo test<CR>", { noremap = true, silent = true })
 
 			-- Cargo build
-			vim.keymap.set("n", "<leader>rb", ":cd %:p:h | !cargo build<CR>", { noremap = true, silent = true })
+			vim.keymap.set("n", "<leader>rb", ":cd %:p:h:h | !cargo build<CR>", { noremap = true, silent = true })
 
 			-- Cargo build --release
 			vim.keymap.set(
@@ -25,15 +30,15 @@ return {
 			)
 
 			-- Cargo check
-			vim.keymap.set("n", "<leader>rc", ":cd %:p:h | !cargo check<CR>", { noremap = true, silent = true })
+			vim.keymap.set("n", "<leader>rc", ":cd %:p:h:h | !cargo check<CR>", { noremap = true, silent = true })
 
 			-- Cargo clippy (linting)
-			vim.keymap.set("n", "<leader>rl", ":cd %:p:h | !cargo clippy<CR>", { noremap = true, silent = true })
+			vim.keymap.set("n", "<leader>rl", ":cd %:p:h:h | !cargo clippy<CR>", { noremap = true, silent = true })
 
 			-- Cargo doc (open docs)
-			vim.keymap.set("n", "<leader>rd", ":cd %:p:h | !cargo doc --open<CR>", { noremap = true, silent = true })
+			vim.keymap.set("n", "<leader>rd", ":cd %:p:h:h | !cargo doc --open<CR>", { noremap = true, silent = true })
 			-- Cargo clean
-			vim.keymap.set("n", "<leader>ra", ":cd %:p:h | !cargo clean <CR>", { noremap = true, silent = true })
+			vim.keymap.set("n", "<leader>ra", ":cd %:p:h:h | !cargo clean <CR>", { noremap = true, silent = true })
 		end,
 	},
 }
