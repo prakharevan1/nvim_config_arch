bc9b6e404de238e0b543d05d761eebf6764519cb
Evan <dagurevan@gmail.com>
2025-03-19 16:55:56 -0700

    Added new, slick dashboard + background + neotree is transparent

--- lua/config/vim-opts.lua
+++ lua/config/vim-opts.lua
@@ -27,3 +27,15 @@ vim.g.clipboard = {
 -- Ensure Lua 5.4 paths are prioritized
 package.path = package.path .. ";/usr/local/share/lua/5.4/?.lua"
 package.cpath = package.cpath .. ";/usr/local/lib/lua/5.4/?.so"
+-- add transparency to neotree and other things, override colorscheme
+vim.api.nvim_create_autocmd("ColorScheme", {
+	pattern = "*",
+	callback = function()
+		vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+		vim.cmd("hi NormalNC guibg=NONE ctermbg=NONE")
+		vim.cmd("hi EndOfBuffer guibg=NONE ctermbg=NONE")
+		vim.cmd("hi NeoTreeNormal guibg=NONE ctermbg=NONE")
+		vim.cmd("hi NeoTreeNormalNC guibg=NONE ctermbg=NONE")
+		vim.cmd("hi NeoTreeEndOfBuffer guibg=NONE ctermbg=NONE")
+	end,
+})
