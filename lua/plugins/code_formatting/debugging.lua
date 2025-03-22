5137c7f9df877c3d6e8481d734e033dd14503132
Evan <dagurevan@gmail.com>
2025-03-22 14:21:30 -0700

    Fixed dap auto close

--- lua/plugins/code_formatting/debugging.lua
+++ lua/plugins/code_formatting/debugging.lua
@@ -17,10 +17,15 @@ return {
 				dapui.open()
 			end
 			dap.listeners.before.event_terminated.dapui_config = function()
-				dapui.close()
+				if not dap.session() then
+					dapui.close()
+				end
 			end
+
 			dap.listeners.before.event_exited.dapui_config = function()
-				dapui.close()
+				if not dap.session() then
+					dapui.close()
+				end
 			end
 			-- rust dap
 			dap.adapters.codelldb = {
