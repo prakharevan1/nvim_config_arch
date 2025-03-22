37f258990dc08c0bf6519e4bbc998b68ddb54588
Evan <dagurevan@gmail.com>
2025-03-22 14:15:00 -0700

    Fixed no run button

--- lua/plugins/code_formatting/debugging.lua
+++ lua/plugins/code_formatting/debugging.lua
@@ -9,7 +9,19 @@ return {
 			local dap = require("dap")
 			local dapui = require("dapui")
 			-- Configure dapui layout and controls to prevent nil element errors
-
+			dapui.setup({})
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
 			-- rust dap
 			dap.adapters.codelldb = {
 				type = "executable",
