80e39dd3f8c4ff3a494a0cd18942ffd1f46dcd77
Evan <dagurevan@gmail.com>
2025-03-29 18:12:13 -0700

    Tried to fix staging error

--- lua/plugins/features/toggle_term.lua
+++ lua/plugins/features/toggle_term.lua
@@ -6,7 +6,7 @@ return {
 
 			-- Fix the keymap with proper escaping for the Lua function
 			vim.keymap.set(
-				{ "n", "t", "v" },
+				{ "n", "v" },
 				"<leader>tt",
 				":lua require('FTerm').toggle()<CR>",
 				{ noremap = true, silent = true, desc = "Toggle the terminal." }
