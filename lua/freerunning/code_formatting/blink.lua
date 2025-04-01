deabc3a5a2842e76f7ca94430481551a090b2a74
Evan <dagurevan@gmail.com>
2025-03-31 17:10:06 -0700

    Fixed blink crashing on bash commands

--- lua/freerunning/code_formatting/blink.lua
+++ lua/freerunning/code_formatting/blink.lua
@@ -27,14 +27,14 @@ return {
 		},
 
 		-- Default list of enabled providers defined so that you can extend it
-		-- elsewhere in your config, without redefining it, due to `opts_extend`
+		-- elsewhere in your config, without redefining it, due to opts_extend
 		sources = {
 			default = { "lsp", "path", "snippets", "buffer" },
 		},
 
 		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+		-- You may use a lua implementation instead by using implementation = "lua" or fallback to the lua implementation,
+		-- when the Rust fuzzy matcher is not available, by using implementation = "prefer_rust"
 		--
 		-- See the fuzzy documentation for more information
 		fuzzy = { implementation = "prefer_rust_with_warning" },
